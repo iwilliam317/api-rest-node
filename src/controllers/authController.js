da86df33e119cebf6d9490300d5eeeecd7f25af9
william <willshinji@gmail.com>
2018-05-04 00:58:20 -0300

    encrypts password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,14 +5,22 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async(request, response) => {
+
+    const { name } = request.body;
+    console.log(name)
       try{
+
+        if (await User.findOne({ email })){
+            return response.status(400).send({ status: 'User already exists!' })
+        }
+
         const user = await User.create(request.body);
         return response.send({ user });
 
         } catch (error){
             return response.status(400).send({ status: 'Registration failed!' })
     }
-})
+});
 
 //route /auth/register
 module.exports = (app) => app.use('/auth', router); 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,35 @@
 const mongoose = require('../database');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema ({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true, 
-    required: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true, 
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+// can't use arrow function
+// it mess 'this'
+UserSchema.pre('save',  async function (next) {
+    const hash = await bcrypt.hash(this.password, 3);
+    this.password = hash;
+
+    next();
 });
 
 const User = mongoose.model('User', UserSchema);
