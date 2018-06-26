3d15bb9c4bf82020c9418f17c699f91b4f1d4568
william <willshinji@gmail.com>
2018-06-26 00:00:45 -0300

    creates file model project

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/auth.js');
 router.use(authMiddleware);
 
 router.get('/', (request, response) => {
-  response.send({ ok: true});
+  response.send({ ok: true, user: request.userId});
 });
 
 module.exports = app => app.use('/projects', router);
