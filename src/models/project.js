d01654deb98d7960d752fded4d9ac5b7ebb3c3b4
william <willshinji@gmail.com>
2018-06-27 00:24:23 -0300

    creates model task

--- src/models/project.js
+++ src/models/project.js
@@ -10,16 +10,18 @@ const ProjectSchema = new mongoose.Schema ({
         type: String,
         require: true,
     },
+
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         require: true,
     },
+
     tasks: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Task',
-    }]
-
+    }],
+    
     createdAt: {
         type: Date,
         default: Date.now,
