0062a55376b3c952d93a8d86537e5d6816fc4e5f
william <willshinji@gmail.com>
2018-06-28 00:37:18 -0300

    creates rest routes for project

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,12 +1,34 @@
 const router = require('express').Router();
 const authMiddleware = require('../middlewares/auth.js');
 
+const Project = require('../models/Project');
+const Task = require('../models/Task');
 
 router.use(authMiddleware);
 
-router.get('/', (request, response) => {
-  response.send({ ok: true, user: request.userId});
+router.get('/', async (request, response) => {
+  response.send({ user: request.userId});
 });
+
+router.get('/:projectId', async (request, response) => {
+  response.send({ user: request.userId});
+});
+
+router.post('/', async (request, response) => {
+  response.send({ user: request.userId});
+});
+
+router.put('/:projectId', async (request, response) => {
+  response.send({ user: request.userId});
+});
+
+router.delete('/:projectId', async (request, response) => {
+  response.send({ user: request.userId});
+});
+
+
+
+
 
 module.exports = app => app.use('/projects', router);
 
