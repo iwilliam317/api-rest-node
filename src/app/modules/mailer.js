090ee9fda33d0036db4a47438f709e69b2313575
william <willshinji@gmail.com>
2018-07-10 21:12:17 -0300

    added notes

--- src/app/modules/mailer.js
+++ src/app/modules/mailer.js
@@ -2,8 +2,11 @@ const nodemailer = require('nodemailer');
 
 const { host, port, user, pass } = require('../config/mail.json');
 
+//shortcut sintax - ES6
 const transport = nodemailer.createTransport({
   host,
   port,
   auth: { user, pass }
 });
+
+module.exports = transport;
